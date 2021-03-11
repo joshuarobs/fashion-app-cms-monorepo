a737f30f22da50a0e374fce500cecf034d31c50c
Joshua Robillos <joshuarobs@icloud.com>
2021-03-11 13:34:47 +1100

    Attempt to fix server to properly serve the built client html files (previously the path was incorrect and wouldn't display anything on the website, but apis worked fine)

--- packages/server-cms/src/index.ts
+++ packages/server-cms/src/index.ts
@@ -4,11 +4,13 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 // const server = require('http').Server(app);
 
+const ROOT_PATH = path.resolve(__dirname, '../../client-cms/build');
+
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../../client-cms/build')));
+app.use(express.static(ROOT_PATH));
 
 app.use('/health', (req: any, res: any) => {
   res.status(200).json({
@@ -24,11 +26,11 @@ app.get('/api', (req: any, res: any) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req: any, res: any) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(ROOT_PATH, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
 //
 // server.listen(port, (error: any) => {
